fff6814a15e69aed8252359f4f9a91e5ee236c8d
pluem <giftsudsoi.2000@gmail.com>
2022-05-26 00:35:38 +0700

    update assign3

--- assign3/pages/index.tsx
+++ assign3/pages/index.tsx
@@ -1,12 +1,49 @@
 import { GetServerSideProps } from "next";
 import axios from 'axios'
 
+let name: String = "Oder Book";
+console.log(name);
+
 const url = 'https://api1.binance.com/api/v3/depth?symbol=BTCUSDT';
 axios.get(url)
   .then(response => {
+    const output = response.data;
+    console.log(output);
+
+
+    function calculateOutputAmount() {
+      let usdtAmount: number = 290000;
+      let totalBTC: number = 0;
+
+      for (let i = 0; i <= 99; i++) {
+        if (usdtAmount > response.data.bids[i][0]) {
+          let price: number = parseFloat(response.data.bids[i][1]);
+          console.log(price);
+
+          usdtAmount = usdtAmount - response.data.bids[i][0];
+          totalBTC += price;
+
+          console.log("Test");
+
+        } else if (usdtAmount < response.data.bids[i][0]) {
+
+          console.log("Test1");
+
+        } else {
+          console.log("test2");
+        }
+      }
+      
+      console.log("เงินต้น :" + "290,000");
+      console.log("เงินทอน : " + usdtAmount);
+      console.log("จำนวนเหรียญ BTC : " + totalBTC);
+    }
+
+    console.log(calculateOutputAmount());
 
   }
-);
+
+  );
 
 export default function IndexPage() {
 
