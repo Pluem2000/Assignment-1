5649c50ca3c7cd1bed1b3634a8da1bdfa967d8c7
pluem <giftsudsoi.2000@gmail.com>
2022-06-16 13:11:21 +0700

    update assign4

--- assign4/pages/index.tsx
+++ assign4/pages/index.tsx
@@ -1,30 +1,144 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
 import * as ethers from "ethers";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getBalance,
+  getChainId,
+  getEthereum,
+  getProvider,
+  getWalletAddress,
+} from "../services/wallet-service";
+import {
+  getNetworkCurrency,
+  getNetworkName,
+  getNetworkTokens,
+} from "../constants/network-id";
+import { formatEther, formatUnits } from "ethers/lib/utils";
+import { Token } from "../types/token.type";
 
 const Home: NextPage = () => {
-
   const [address, setAddress] = useState<string | null>(null);
+  const [network, setNetwork] = useState<string | null>(null);
+  const [balance, setBalance] = useState<string | null>(null);
 
-  const connectWallet = () => {
-    if (typeof window.ethereum !== "undefined" && window.ethereum.request) {
-      window.ethereum.request({ method: "eth_requestAccounts" });
+  const [tokenBalances, setTokenBalances] = useState<Record<string, string>>(
+    {}
+  );
+
+  const getTokenBalance = async (
+    tokenAddress: string,
+    ownerAddress: string
+  ) => {
+    const abi = ["function balanceOf(address owner) view returns (uint256)"];
+    const contract = new ethers.Contract(tokenAddress, abi, getProvider()!);
+    return contract.balanceOf(ownerAddress);
+  };
+
+  const addTokenToWallet = async (token: Token) => {
+    try {
+      
+      const wasAdded = await window.ethereum.request({
+        method: "wallet_watchAsset",
+        params: {
+          type: "ERC20", 
+          options: {
+            address: token.address, 
+            symbol: token.symbol, 
+            decimals: token.decimals, 
+            image: token.imageUrl, 
+          },
+        },
+      });
+
+      if (wasAdded) {
+        console.log("Thanks for your interest!");
+      } else {
+        console.log("Your loss!");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
+  const loadAccountData = async () => {
+    const addr = getWalletAddress();
+    setAddress(addr);
+
+    const chainId = await getChainId();
+    setNetwork(chainId);
+
+    const bal = await getBalance(addr);
+    if (bal) setBalance(formatEther(bal));
+
+    const tokenList = getNetworkTokens(chainId);
+
+    const tokenBalList = await Promise.all(
+      tokenList.map((token) =>
+        getTokenBalance(token.address, addr).then((res) =>
+          formatUnits(res, token.decimals)
+        )
+      )
+    );
+
+    tokenList.forEach((token, i) => {
+      tokenBalances[token.symbol] = tokenBalList[i];
+    });
+    setTokenBalances({ ...tokenBalances });
+  };
+
   useEffect(() => {
-    if (typeof window.ethereum !== "undefined") {
-      const walletAddress = (window.ethereum as any).selectedAddress;
-      setAddress(walletAddress);
-    }
+    loadAccountData();
+
+    const handleAccountChange = (addresses: string[]) => {
+      setAddress(addresses[0]);
+      loadAccountData();
+    };
+
+    const handleNetworkChange = (networkId: string) => {
+      setNetwork(networkId);
+      loadAccountData();
+    };
+
+    getEthereum()?.on("accountsChanged", handleAccountChange);
+
+    getEthereum()?.on("chainChanged", handleNetworkChange);
   }, []);
 
   return (
     <div>
-      <p>Hello World</p>
-
       {address ? (
-        <p>Your wallet address is {address}</p>
+        <div>
+          <p>Your wallet address is {address}</p>
+          <p>
+            Current network is {getNetworkName(network)} ({network})
+          </p>
+          <p>
+            Your balance is {balance} {getNetworkCurrency(network)}
+          </p>
+          <h4 className="font-bold text-lg">Token list</h4>
+          <div>
+            {getNetworkTokens(network).map((token) => (
+              <div key={token.symbol} className="flex mb-4">
+                <div>
+                  <img
+                    onClick={() => addTokenToWallet(token)}
+                    src={token.imageUrl}
+                    className="w-12 h-12 mr-8 cursor-pointer"
+                  />
+                </div>
+                <div>
+                  <div>
+                    {token.name} ({token.symbol})
+                  </div>
+                  <div>
+                    {tokenBalances[token.symbol] || 0} {token.symbol}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       ) : (
         <button
           type="button"
@@ -33,10 +147,9 @@ const Home: NextPage = () => {
         >
           Connect
         </button>
-      )
-      }
+      )}
     </div>
   );
 };
 
-export default Home
+export default Home;
